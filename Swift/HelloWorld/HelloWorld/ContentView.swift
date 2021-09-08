0429c9229f971e19d6f42ab14cc2e9b0605e0e66
WilliamHJF <jihuo@ucsd.edu>
2021-09-07 17:06:13 -0700

    Code Submit

---
+++ Swift/HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by 霍一帆 on 8/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
